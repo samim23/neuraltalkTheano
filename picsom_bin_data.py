777067ee64e11406719fd972b2d25dde8513f1e2
Rakshith Shetty <shettyr1@login2.local>
2015-04-03 15:48:06 +0300

    Using numpy to load binary features as reading in loop was taking too much memory

--- picsom_bin_data.py
+++ picsom_bin_data.py
@@ -2,6 +2,7 @@
 
 from struct import unpack
 import os
+import numpy as np
 
 class picsom_bin_data:
 
@@ -40,19 +41,24 @@ class picsom_bin_data:
   def get_float_list(self, iL) :
     if iL == -1 :
 	  iL = xrange(self._nobjects)
+          vec = np.fromfile(self._fp, dtype = np.float32, count=self._nobjects*self._vdim )
+          vec = vec.reshape(self._nobjects,self._vdim)
+	  print vec.shape
+    else:
 	
-    vec = [[]]*len(iL)
-    for idx, i in enumerate(iL):
-      if (i<0 or i>=self._nobjects) :
-        raise Exception('Index %i exceeds size of "%s"' % (i, self._path))
-      self._fp.seek(self._hsize+i*self._rlength)
-      vec[idx] = list(unpack('%if' % self._vdim, self._fp.read(self._rlength)))
+      vec = [[]]*len(iL)
+      for idx, i in enumerate(iL):
+        if (i<0 or i>=self._nobjects) :
+          raise Exception('Index %i exceeds size of "%s"' % (i, self._path))
+        self._fp.seek(self._hsize+i*self._rlength)
+        vec[idx] = list(unpack('%if' % self._vdim, self._fp.read(self._rlength)))
     return vec
 
 if __name__ == "__main__":
-  d = picsom_bin_data("/triton/ics/project/imagedb/picsom/databases/v/features/c_in12_z_fc6_a_ca3.bin");
+  d = picsom_bin_data("/proj/mediaind/picsom/databases/COCO/features/c_in14_o6_fc6_d_a.bin")
   print d.vdim()
   print d.nobjects()
-  print d.get_float(10)[0:10]
+  print d.get_float_list(-1)[1][0:10]
+  #print d.get_float(1)[0:10]
 #  print d.get_float(10)
 
